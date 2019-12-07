6d332febc41f0da5b64e4bf5676323a3e201ccb2
Gordey Doronin <g.doronin@gmail.com>
2019-12-07 14:28:20 +0700

    Add flatlist functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { listVersions, listVersionNumbers, listVersionBuilds } from './core';
-export { listVersions, listVersionNumbers, listVersionBuilds };
+import { listVersions, listVersionNumbers, listVersionBuilds, flatlistVersionNumbers, flatlistVersionBuilds } from './core';
+export { listVersions, listVersionNumbers, listVersionBuilds, flatlistVersionNumbers, flatlistVersionBuilds };
 
 // json data
 export { default as iosVersionHistory } from './ios-version-history';
@@ -7,6 +7,9 @@ export { default as macosVersionHistory } from './macos-version-history';
 export { default as tvosVersionHistory } from './tvos-version-history';
 
 // listVersions
+export function listIosVersions(): string[] { return listVersions('ios'); }
+export function listMacosVersions(): string[] { return listVersions('macos'); }
+export function listTvosVersions(): string[] { return listVersions('tvos'); }
 export function flatlistIosVersions(): string[] { return listVersions('ios'); }
 export function flatlistMacosVersions(): string[] { return listVersions('macos'); }
 export function flatlistTvosVersions(): string[] { return listVersions('tvos'); }
@@ -15,14 +18,18 @@ export function flatlistTvosVersions(): string[] { return listVersions('tvos'); 
 export function listIosVersionsNumbers(): string[][] { return listVersionNumbers('ios'); }
 export function listMacosVersionsNumbers(): string[][] { return listVersionNumbers('macos'); }
 export function listTvosVersionsNumbers(): string[][] { return listVersionNumbers('tvos'); }
-export function flatlistIosVersionsNumbers(): string[] { return ([] as string[]).concat(...listVersionNumbers('ios')); }
-export function flatlistMacosVersionsNumbers(): string[] { return ([] as string[]).concat(...listVersionNumbers('macos')); }
-export function flatlistTvosVersionsNumbers(): string[] { return ([] as string[]).concat(...listVersionNumbers('tvos')); }
+
+// flatlistVersionNumbers
+export function flatlistIosVersionsNumbers(): string[] { return flatlistVersionNumbers('ios'); }
+export function flatlistMacosVersionsNumbers(): string[] { return flatlistVersionNumbers('macos'); }
+export function flatlistTvosVersionsNumbers(): string[] { return flatlistVersionNumbers('tvos'); }
 
 // listVersionBuilds
 export function listIosVersionsBuilds(): string[][] { return listVersionBuilds('ios'); }
 export function listMacosVersionsBuilds(): string[][] { return listVersionBuilds('macos'); }
 export function listTvosVersionsBuilds(): string[][] { return listVersionBuilds('tvos'); }
-export function flatlistIosVersionsBuilds(): string[] { return ([] as string[]).concat(...listVersionBuilds('ios')); }
-export function flatlistMacosVersionsBuilds(): string[] { return ([] as string[]).concat(...listVersionBuilds('macos')); }
-export function flatlistTvosVersionsBuilds(): string[] { return ([] as string[]).concat(...listVersionBuilds('tvos')); }
+
+// flatlistVersionBuilds
+export function flatlistIosVersionsBuilds(): string[] { return flatlistVersionBuilds('ios'); }
+export function flatlistMacosVersionsBuilds(): string[] { return flatlistVersionBuilds('macos'); }
+export function flatlistTvosVersionsBuilds(): string[] { return flatlistVersionBuilds('tvos'); }
