fbc895b114a8a6ab9996765422c4278be9fe24d5
Gordey Doronin <gordey4doronin@gmail.com>
2021-04-13 19:32:35 +0200

    Fix postbuild script

--- .build/postbuild.sh
+++ .build/postbuild.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Copy files required for package publishing
 cp package.json dist/src/ && cp README.md dist/src/
-sed -i "" "s/exit -1/exit 0/" "dist/src/package.json"
+
+# Replace guards (used to prevent publishing from the root directory)
+sed -i.bak 's/exit -1/exit 0/' "dist/src/package.json" && rm "dist/src/package.json.bak"
