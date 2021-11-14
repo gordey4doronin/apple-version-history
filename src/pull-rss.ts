2179f8bb13a512952989f7a48d77f4c22a55cc33
Gordey Doronin <gordey4doronin@gmail.com>
2021-11-14 16:10:44 +0100

    Experimenting with debug colors to read logs faster

--- src/pull-rss.ts
+++ src/pull-rss.ts
@@ -11,6 +11,17 @@ import { versionNumberWithoutPatch, addMinorZero } from './util'
 const appleRssUrl = 'https://developer.apple.com/news/releases/rss/releases.rss'
 const debug = require.main === module
 
+const colors = {
+  black: (string) => `\x1b[30m${string}\x1b[39m`,
+  red: (string) => `\x1b[31m${string}\x1b[39m`,
+  green: (string) => `\x1b[32m${string}\x1b[39m`,
+  yellow: (string) => `\x1b[33m${string}\x1b[39m`,
+  blue: (string) => `\x1b[34m${string}\x1b[39m`,
+  magenta: (string) => `\x1b[35m${string}\x1b[39m`,
+  cyan: (string) => `\x1b[36m${string}\x1b[39m`,
+  white: (string) => `\x1b[37m${string}\x1b[39m`
+}
+
 // Run script only when run directly from command line
 if (require.main === module) {
   const start = Date.now()
@@ -96,30 +107,35 @@ export const getTitles = (rssItems) => rssItems.map(x => x.title[0])
 /**
  * Filters titles from Apple RSS feed items.
  */
-export const filterTitles = (titles) => titles
-  .filter(title => {
+export function filterTitles (titles) {
+  debug && console.log('')
+
+  return titles.filter(title => {
     const match = title.match(filterRegex)
 
     if (match) {
-      debug && console.log(`+ Keep title="${title}" with match="${match}"`)
+      debug && console.log(colors.green(`+ Keep title="${title}" with match="${match}"`))
     } else {
-      debug && console.log(`- Remove title="${title}" with match="${match}"`)
+      debug && console.log(colors.red(`- Ignore title="${title}" with match="${match}"`))
     }
 
     return match
   })
+}
 
 /**
  * Parses titles from Apple RSS feed items.
  */
-export const parseTitles = (titles) => titles
-  .map(title => {
+export function parseTitles (titles) {
+  debug && console.log('')
+
+  return titles.map(title => {
     const match = title.match(parseRegex)
 
     if (!match) {
       throw new Error(`Could not parse title "${title}`)
     }
-    debug && console.log(`Parsed title="${title}" with match="${match}"`)
+    debug && console.log(`Parsed title="${colors.yellow(title)}" with match="${match}"`)
 
     return match
   })
@@ -131,6 +147,7 @@ export const parseTitles = (titles) => titles
     cycle: cycle || null,
     build
   }))
+}
 
 /**
  * Applies RSS changes to existing OS objects.
@@ -155,8 +172,10 @@ export async function applyRssChanges(rssTitles, paths = {
     'watchos': JSON.parse(files[3])
   }
 
+  debug && console.log('')
+
   rssTitles.forEach(({ os, version, build, codename }) => {
-    debug && console.log(`Processing ${os} ${version} ${build}`)
+    debug && console.log(`Processing ${colors.blue(os)} ${colors.magenta(version)} ${colors.cyan(build)}`)
 
     // Assume that iOS and iPadOS are the same for now.
     // TODO: Figure out later.
@@ -179,7 +198,7 @@ export async function applyRssChanges(rssTitles, paths = {
     // versionName doesn't exist => init new object
     if (!json[versionName]) {
       json[versionName] = {}
-      debug && console.log(`${os} versionName ${versionName} doesn't exit => init new object`)
+      debug && console.log(`${colors.blue(os)} versionName ${colors.magenta(versionName)} doesn't exit => init new object`)
     }
 
     // version doesn't exist => add version + build
@@ -187,11 +206,11 @@ export async function applyRssChanges(rssTitles, paths = {
       if (isDualBuild(build)) {
         const [build1, build2] = splitDualBuild(build)
         json[versionName][version] = [ build1, build2 ]
-        debug && console.log(`${os} version ${version} doesn't exist => add version + build ${version} [ ${build1} ]`)
-        debug && console.log(`${os} version ${version} doesn't exist => add version + build ${version} [ ${build2} ]`)
+        debug && console.log(`${colors.blue(os)} version ${colors.magenta(version)} doesn't exist => add version + build ${colors.magenta(version)} [ ${colors.cyan(build1)} ]`)
+        debug && console.log(`${colors.blue(os)} version ${colors.magenta(version)} doesn't exist => add version + build ${colors.magenta(version)} [ ${colors.cyan(build2)} ]`)
       } else {
         json[versionName][version] = [ build ]
-        debug && console.log(`${os} version ${version} doesn't exist => add version + build ${version} [ ${build} ]`)
+        debug && console.log(`${colors.blue(os)} version ${colors.magenta(version)} doesn't exist => add version + build ${colors.magenta(version)} [ ${colors.cyan(build)} ]`)
       }
     }
 
@@ -199,7 +218,7 @@ export async function applyRssChanges(rssTitles, paths = {
       // build doesn't exist => add build
       if (!json[versionName][version].includes(build)) {
         json[versionName][version].push(build)
-        debug && console.log(`${os} ${version} build ${build} doesn't exist => add build ${build}`)
+        debug && console.log(`${colors.blue(os)} ${colors.magenta(version)} build ${colors.cyan(build)} doesn't exist => add build ${colors.cyan(build)}`)
       }
     } else if (isDualBuild(build)) {
       const [build1, build2] = splitDualBuild(build)
@@ -207,13 +226,13 @@ export async function applyRssChanges(rssTitles, paths = {
       // build doesn't exist => add build
       if (!json[versionName][version].includes(build1)) {
         json[versionName][version].push(build1)
-        debug && console.log(`${os} ${version} build ${build1} doesn't exist => add build ${build1}`)
+        debug && console.log(`${colors.blue(os)} ${colors.magenta(version)} build ${colors.cyan(build1)} doesn't exist => add build ${colors.cyan(build1)}`)
       }
 
       // build doesn't exist => add build
       if (!json[versionName][version].includes(build2)) {
         json[versionName][version].push(build2)
-        debug && console.log(`${os} ${version} build ${build2} doesn't exist => add build ${build2}`)
+        debug && console.log(`${colors.blue(os)} ${colors.magenta(version)} build ${colors.cyan(build2)} doesn't exist => add build ${colors.cyan(build2)}`)
       }
     }
 
