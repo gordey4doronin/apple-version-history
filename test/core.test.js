aa4b4f208cc5c80963af1bf9097d3834e48b754f
Gordey Doronin <gordey4doronin@gmail.com>
2024-05-17 12:10:25 +0200

    Fix indentation

--- test/core.test.js
+++ test/core.test.js
@@ -6,90 +6,90 @@ const flatlistVersionBuilds = core.flatlistVersionBuilds
 const flatlistBuilds = core.flatlistBuilds
 
 describe('core', () => {
-    describe('#listVersions()', () => {
-        it('returns ios versions array', () => {
-            assert.includeMembers(listVersions('ios'), ['iPhone OS 1.0.x', 'iOS 8.1.x'])
-        })
-
-        it('returns macos versions array', () => {
-            assert.includeMembers(listVersions('macos'), ['Mac OS X 10.0.x', 'OS X 10.9.x', 'macOS 10.12.x'])
-        })
-
-        it('returns tvos versions array', () => {
-            assert.includeMembers(listVersions('tvos'), ['tvOS 9.x'])
-        })
+  describe('#listVersions()', () => {
+    it('returns ios versions array', () => {
+      assert.includeMembers(listVersions('ios'), ['iPhone OS 1.0.x', 'iOS 8.1.x'])
     })
 
-    describe('#flatlistVersionNumbers()', () => {
-        it('returns ios version numbers flattened array', () => {
-            assert.includeMembers(flatlistVersionNumbers('ios'), ['iPhone OS 1.0', 'iPhone OS 1.0.1', 'iOS 8.1', 'iOS 8.1.1'])
-        })
-
-        it('returns macos version numbers flattened array', () => {
-            assert.includeMembers(flatlistVersionNumbers('macos'), [
-                'Mac OS X 10.0', 'Mac OS X 10.0.1',
-                'OS X 10.9', 'OS X 10.9.1',
-                'macOS 10.12', 'macOS 10.12.1'
-            ])
-        })
-
-        it('returns tvos version numbers flattened array', () => {
-            assert.includeMembers(flatlistVersionNumbers('tvos'), ['tvOS 9.0', 'tvOS 9.0.1'])
-        })
+    it('returns macos versions array', () => {
+      assert.includeMembers(listVersions('macos'), ['Mac OS X 10.0.x', 'OS X 10.9.x', 'macOS 10.12.x'])
     })
 
-    describe('#flatlistVersionBuilds()', () => {
-        it('returns ios version builds flattened array', () => {
-            assert.includeMembers(flatlistVersionBuilds('ios'), [
-                'iPhone OS 1.0.1 (1C25)', 'iPhone OS 1.1.4 (4A102)',
-                'iOS 8.1 (12B410)', 'iOS 8.1 (12B411)', 'iOS 8.1.1 (12B435)', 'iOS 8.1.1 (12B436)'
-            ])
-        })
+    it('returns tvos versions array', () => {
+      assert.includeMembers(listVersions('tvos'), ['tvOS 9.x'])
+    })
+  })
 
-        it('returns macos version builds flattened array', () => {
-            assert.includeMembers(flatlistVersionBuilds('macos'), [
-                'Mac OS X 10.0 (4K78)', 'Mac OS X 10.0.1 (4L13)',
-                'OS X 10.9 (13A603)', 'OS X 10.9.1 (13B42)', 'OS X 10.9.2 (13C64)', 'OS X 10.9.2 (13C1021)',
-                'macOS 10.12 (16A323)', 'macOS 10.12.1 (16B2555)', 'macOS 10.12.1 (16B2657)'
-            ])
-        })
-
-        it('returns tvos version builds flattened array', () => {
-            assert.includeMembers(flatlistVersionBuilds('tvos'), ['tvOS 9.0 (13T396)', 'tvOS 9.0.1 (13T402)'])
-        })
+  describe('#flatlistVersionNumbers()', () => {
+    it('returns ios version numbers flattened array', () => {
+      assert.includeMembers(flatlistVersionNumbers('ios'), ['iPhone OS 1.0', 'iPhone OS 1.0.1', 'iOS 8.1', 'iOS 8.1.1'])
     })
 
-    describe('#flatlistBuilds()', () => {
-        it('returns ios builds flattened array', () => {
-            assert.includeMembers(flatlistBuilds('ios'), [
-                '1C25',
-                '4A102',
-                '12B410',
-                '12B411',
-                '12B435',
-                '12B436'
-            ])
-        })
-
-        it('returns macos builds flattened array', () => {
-            assert.includeMembers(flatlistBuilds('macos'), [
-                '4K78',
-                '4L13',
-                '13A603',
-                '13B42',
-                '13C64',
-                '13C1021',
-                '16A323',
-                '16B2555',
-                '16B2657'
-            ])
-        })
-
-        it('returns tvos builds flattened array', () => {
-            assert.includeMembers(flatlistBuilds('tvos'), [
-                '13T396',
-                '13T402'
-            ])
-        })
+    it('returns macos version numbers flattened array', () => {
+      assert.includeMembers(flatlistVersionNumbers('macos'), [
+        'Mac OS X 10.0', 'Mac OS X 10.0.1',
+        'OS X 10.9', 'OS X 10.9.1',
+        'macOS 10.12', 'macOS 10.12.1'
+      ])
     })
+
+    it('returns tvos version numbers flattened array', () => {
+      assert.includeMembers(flatlistVersionNumbers('tvos'), ['tvOS 9.0', 'tvOS 9.0.1'])
+    })
+  })
+
+  describe('#flatlistVersionBuilds()', () => {
+    it('returns ios version builds flattened array', () => {
+      assert.includeMembers(flatlistVersionBuilds('ios'), [
+        'iPhone OS 1.0.1 (1C25)', 'iPhone OS 1.1.4 (4A102)',
+        'iOS 8.1 (12B410)', 'iOS 8.1 (12B411)', 'iOS 8.1.1 (12B435)', 'iOS 8.1.1 (12B436)'
+      ])
+    })
+
+    it('returns macos version builds flattened array', () => {
+      assert.includeMembers(flatlistVersionBuilds('macos'), [
+        'Mac OS X 10.0 (4K78)', 'Mac OS X 10.0.1 (4L13)',
+        'OS X 10.9 (13A603)', 'OS X 10.9.1 (13B42)', 'OS X 10.9.2 (13C64)', 'OS X 10.9.2 (13C1021)',
+        'macOS 10.12 (16A323)', 'macOS 10.12.1 (16B2555)', 'macOS 10.12.1 (16B2657)'
+      ])
+    })
+
+    it('returns tvos version builds flattened array', () => {
+      assert.includeMembers(flatlistVersionBuilds('tvos'), ['tvOS 9.0 (13T396)', 'tvOS 9.0.1 (13T402)'])
+    })
+  })
+
+  describe('#flatlistBuilds()', () => {
+    it('returns ios builds flattened array', () => {
+      assert.includeMembers(flatlistBuilds('ios'), [
+        '1C25',
+        '4A102',
+        '12B410',
+        '12B411',
+        '12B435',
+        '12B436'
+      ])
+    })
+
+    it('returns macos builds flattened array', () => {
+      assert.includeMembers(flatlistBuilds('macos'), [
+        '4K78',
+        '4L13',
+        '13A603',
+        '13B42',
+        '13C64',
+        '13C1021',
+        '16A323',
+        '16B2555',
+        '16B2657'
+      ])
+    })
+
+    it('returns tvos builds flattened array', () => {
+      assert.includeMembers(flatlistBuilds('tvos'), [
+        '13T396',
+        '13T402'
+      ])
+    })
+  })
 })
--- test/util.test.js
+++ test/util.test.js
@@ -7,73 +7,73 @@ const versionNumberWithoutPatch = util.versionNumberWithoutPatch
 const addMinorZero = util.addMinorZero
 
 describe('utils', () => {
-    describe('#pickJson()', () => {
-        it('returns ios object containing some keys', () => {
-            assert.containsAllKeys(pickJson('ios'), ['iPhone OS 1.0.x', 'iOS 8.1.x'])
-        })
-
-        it('returns macos object containing some keys', () => {
-            assert.containsAllKeys(pickJson('macos'), ['Mac OS X 10.0.x', 'OS X 10.9.x', 'macOS 10.12.x'])
-        })
-
-        it('returns tvos object containing some keys', () => {
-            assert.containsAllKeys(pickJson('tvos'), ['tvOS 9.x'])
-        })
+  describe('#pickJson()', () => {
+    it('returns ios object containing some keys', () => {
+      assert.containsAllKeys(pickJson('ios'), ['iPhone OS 1.0.x', 'iOS 8.1.x'])
     })
 
-    describe('#versionNameWithoutSuffix()', () => {
-        it('parses ios version names', () => {
-            assert.equal(versionNameWithoutSuffix('iPhone OS 1.0.x'), 'iPhone OS')
-            assert.equal(versionNameWithoutSuffix('iOS 8.1.x'), 'iOS')
-        })
-
-        it('parses macos version names', () => {
-            assert.equal(versionNameWithoutSuffix('Mac OS X 10.0.x'), 'Mac OS X')
-            assert.equal(versionNameWithoutSuffix('OS X 10.9.x'), 'OS X')
-            assert.equal(versionNameWithoutSuffix('macOS 10.12.x'), 'macOS')
-        })
-
-        it('parses tvos version names', () => {
-            assert.equal(versionNameWithoutSuffix('tvOS 9.x'), 'tvOS')
-        })
+    it('returns macos object containing some keys', () => {
+      assert.containsAllKeys(pickJson('macos'), ['Mac OS X 10.0.x', 'OS X 10.9.x', 'macOS 10.12.x'])
     })
 
-    describe('#versionNumberWithoutPatch()', () => {
-        it('returns version number without patch part', () => {
-            assert.equal(versionNumberWithoutPatch('1.0.2'), '1.0')
-            assert.equal(versionNumberWithoutPatch('100.000.200'), '100.000')
-        })
+    it('returns tvos object containing some keys', () => {
+      assert.containsAllKeys(pickJson('tvos'), ['tvOS 9.x'])
+    })
+  })
 
-        it('returns the same version number if there is patch part', () => {
-            assert.equal(versionNumberWithoutPatch('1.0'), '1.0')
-            assert.equal(versionNumberWithoutPatch('100.000'), '100.000')
-        })
+  describe('#versionNameWithoutSuffix()', () => {
+    it('parses ios version names', () => {
+      assert.equal(versionNameWithoutSuffix('iPhone OS 1.0.x'), 'iPhone OS')
+      assert.equal(versionNameWithoutSuffix('iOS 8.1.x'), 'iOS')
     })
 
-    describe('#addMinorZero()', () => {
-        it('returns version with minor zero part', () => {
-            assert.equal(addMinorZero('1'), '1.0')
-            assert.equal(addMinorZero('100'), '100.0')
-        })
-
-        it('returns the same version if there is minor part', () => {
-            assert.equal(addMinorZero('1.0'), '1.0')
-            assert.equal(addMinorZero('100.000'), '100.000')
-        })
-
-        it('returns the same version if there is patch part', () => {
-            assert.equal(addMinorZero('1.0.2'), '1.0.2')
-            assert.equal(addMinorZero('100.000.200'), '100.000.200')
-        })
+    it('parses macos version names', () => {
+      assert.equal(versionNameWithoutSuffix('Mac OS X 10.0.x'), 'Mac OS X')
+      assert.equal(versionNameWithoutSuffix('OS X 10.9.x'), 'OS X')
+      assert.equal(versionNameWithoutSuffix('macOS 10.12.x'), 'macOS')
     })
 
-    describe('#hasPatch()', () => {
-        it('returns false when no patch part', () => {
-            assert.isFalse(hasPatch('10.0'))
-        })
-
-        it('returns true when there is patch part', () => {
-            assert.isTrue(hasPatch('10.0.1'))
-        })
+    it('parses tvos version names', () => {
+      assert.equal(versionNameWithoutSuffix('tvOS 9.x'), 'tvOS')
     })
+  })
+
+  describe('#versionNumberWithoutPatch()', () => {
+    it('returns version number without patch part', () => {
+      assert.equal(versionNumberWithoutPatch('1.0.2'), '1.0')
+      assert.equal(versionNumberWithoutPatch('100.000.200'), '100.000')
+    })
+
+    it('returns the same version number if there is patch part', () => {
+      assert.equal(versionNumberWithoutPatch('1.0'), '1.0')
+      assert.equal(versionNumberWithoutPatch('100.000'), '100.000')
+    })
+  })
+
+  describe('#addMinorZero()', () => {
+    it('returns version with minor zero part', () => {
+      assert.equal(addMinorZero('1'), '1.0')
+      assert.equal(addMinorZero('100'), '100.0')
+    })
+
+    it('returns the same version if there is minor part', () => {
+      assert.equal(addMinorZero('1.0'), '1.0')
+      assert.equal(addMinorZero('100.000'), '100.000')
+    })
+
+    it('returns the same version if there is patch part', () => {
+      assert.equal(addMinorZero('1.0.2'), '1.0.2')
+      assert.equal(addMinorZero('100.000.200'), '100.000.200')
+    })
+  })
+
+  describe('#hasPatch()', () => {
+    it('returns false when no patch part', () => {
+      assert.isFalse(hasPatch('10.0'))
+    })
+
+    it('returns true when there is patch part', () => {
+      assert.isTrue(hasPatch('10.0.1'))
+    })
+  })
 })
